e4dcb52edb88e56531ea16f4421270dfa6eb3963
sammyboy187 <144049436+sammyboy187@users.noreply.github.com>
2023-09-04 18:42:21 +0200

    Update uvoter.py

--- uvoter.py
+++ uvoter.py
@@ -5,7 +5,7 @@ post_min = 7490000
 post_max = 7491769
 user_agent = "SammyVoter 1.0"
 
+cookies = {"_forum_session": forum_session}
 for i in range(post_min, post_max):
     print(i)
-    cookies = {"_forum_session": forum_session}
     r = requests.get("https://www.mikrocontroller.net/topic/vote_post?id=%d&rating=1" % (i,), headers={"User-Agent": user_agent}, cookies=cookies)
