d4bb5a20e9d10baf9e458ec14db966552bd41d31
sammyboy187 <144049436+sammyboy187@users.noreply.github.com>
2023-09-06 19:00:15 +0200

    Update uvoter2.js

--- uvoter2.js
+++ uvoter2.js
@@ -7,7 +7,7 @@
 
 function getHTTP(url) {
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", url, false ); // false for synchronous request
+	xmlHttp.open( "GET", url, true ); // false for synchronous request
 	xmlHttp.send( null );
 }
 
